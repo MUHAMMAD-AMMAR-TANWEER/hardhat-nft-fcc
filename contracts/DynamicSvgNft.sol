e18cbb3d6039f657d952bf54530214fcb62a0f04
root <root@DESKTOP-GNFMHLV.localdomain>
2022-11-12 17:55:27 -0800

    Test are also written

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -1,44 +1,69 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "base64-sol/base64.sol";
+import "hardhat/console.sol";
 
 error ERC721Metadata__URI_QueryFor_NonExistentToken();
 
-contract DynamicSvgNft is ERC721 {
+contract DynamicSvgNft is ERC721, Ownable {
     uint256 private s_tokenCounter;
     string private s_lowImageURI;
     string private s_highImageURI;
-    mapping(uint256 => int256) private s_tokenIdToHighValue;
-    AggregatorV3Interface internal immutable i_priceFeed;
 
+    mapping(uint256 => int256) private s_tokenIdToHighValues;
+    AggregatorV3Interface internal immutable i_priceFeed;
     event CreatedNFT(uint256 indexed tokenId, int256 highValue);
 
     constructor(
         address priceFeedAddress,
         string memory lowSvg,
         string memory highSvg
-    ) ERC721("DynamicSvgNft ", "DSN") {
-        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
+    ) ERC721("Dynamic SVG NFT", "DSN") {
         s_tokenCounter = 0;
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
+        // setLowSVG(lowSvg);
+        // setHighSVG(highSvg);
         s_lowImageURI = svgToImageURI(lowSvg);
         s_highImageURI = svgToImageURI(highSvg);
     }
 
-    function svgToImageURI(string memory svg) public pure returns (string memory) {
-        string memory baseURL = "data:image/svg+xml;base64,";
-        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
-        return string(abi.encodePacked(baseURL, svgBase64Encoded));
-    }
+    // function setLowURI(string memory svgLowURI) public onlyOwner {
+    //     s_lowImageURI = svgLowURI;
+    // }
+
+    // function setHighURI(string memory svgHighURI) public onlyOwner {
+    //     s_highImageURI = svgHighURI;
+    // }
+
+    // function setLowSVG(string memory svgLowRaw) public onlyOwner {
+    //     string memory svgURI = svgToImageURI(svgLowRaw);
+    //     setLowURI(svgURI);
+    // }
+
+    // function setHighSVG(string memory svgHighRaw) public onlyOwner {
+    //     string memory svgURI = svgToImageURI(svgHighRaw);
+    //     setHighURI(svgURI);
+    // }
 
     function mintNft(int256 highValue) public {
-        s_tokenIdToHighValue[s_tokenCounter] = highValue;
+        s_tokenIdToHighValues[s_tokenCounter] = highValue;
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter = s_tokenCounter + 1;
         emit CreatedNFT(s_tokenCounter, highValue);
+    }
+
+    // You could also just upload the raw SVG and have solildity convert it!
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        // example:
+        // '<svg width="500" height="500" viewBox="0 0 285 350" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M150,0,L75,200,L225,200,Z"></path></svg>'
+        // would return ""
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
     }
 
     function _baseURI() internal pure override returns (string memory) {
@@ -49,13 +74,11 @@ contract DynamicSvgNft is ERC721 {
         if (!_exists(tokenId)) {
             revert ERC721Metadata__URI_QueryFor_NonExistentToken();
         }
-
         (, int256 price, , , ) = i_priceFeed.latestRoundData();
         string memory imageURI = s_lowImageURI;
-        if (price >= s_tokenIdToHighValue[tokenId]) {
+        if (price >= s_tokenIdToHighValues[tokenId]) {
             imageURI = s_highImageURI;
         }
-
         return
             string(
                 abi.encodePacked(
@@ -76,11 +99,11 @@ contract DynamicSvgNft is ERC721 {
             );
     }
 
-    function getLowSvg() public view returns (string memory) {
+    function getLowSVG() public view returns (string memory) {
         return s_lowImageURI;
     }
 
-    function getHighSvg() public view returns (string memory) {
+    function getHighSVG() public view returns (string memory) {
         return s_highImageURI;
     }
 
